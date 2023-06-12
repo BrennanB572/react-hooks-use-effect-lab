9660fe002327b75d2ed79ad2c7babe8c6bae5834
BrennanB572 <Brennanb572@gmail.com>
2023-06-11 21:06:36 -0700

    lab completed

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,9 +1,21 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
   // add useEffect code
+
+useEffect(() => {
+  const timeoutId = setTimeout(() => setTimeRemaining(timeRemaining - 1), 1000)
+    if (timeRemaining === 0) {
+      setTimeRemaining(10);
+      onAnswered(false);
+    }
+    return function cleanup() {
+      clearTimeout(timeoutId)
+    }
+  },)
+
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
@@ -27,6 +39,6 @@ function Question({ question, onAnswered }) {
       <h5>{timeRemaining} seconds remaining</h5>
     </>
   );
-}
+    }
 
 export default Question;
